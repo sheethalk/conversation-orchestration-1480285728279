f45d0b9df7d6397e9c0b5d01f434577c1f6851aa
Sheethal Kumar <sheethal.kumar@accenture.com>
2016-11-27 16:56:20 -0600

    updated the messages

--- app.js
+++ app.js
@@ -3,20 +3,37 @@
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
-
+require( 'dotenv' ).config( {silent: true} );
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var bodyParser = require( 'body-parser' );
+var Watson = require( 'watson-developer-cloud/conversation/v1' );  // watson sdk
+var uuid = require( 'uuid' );
+var vcapServices = require( 'vcap_services' );
+var basicAuth = require( 'basic-auth-connect' );
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
+var logs = null;
 // create a new express server
 var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.use( bodyParser.json() );
+
+// Create the service wrapper
+var conversation = new Watson( {
+  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+  // username: '<username>',
+  // password: '<password>',
+  url: 'https://gateway.watsonplatform.net/conversation/api',
+  version_date: '2016-09-20',
+  version: 'v1'
+} );
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -26,3 +43,83 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+// Endpoint to be call from the client side
+app.post( '/api/message', function(req, res) {
+  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+  if ( !workspace || workspace === '<workspace-id>' ) {
+    return res.json( {
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' +
+        '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' +
+        'Once a workspace has been defined the intents may be imported from ' +
+        '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    } );
+  }
+  var payload = {
+    workspace_id: workspace,
+    context: {},
+    input: {}
+  };
+  if ( req.body ) {
+    if ( req.body.input ) {
+      payload.input = req.body.input;
+    }
+    if ( req.body.context ) {
+      // The client must maintain context/state
+      payload.context = req.body.context;
+    }
+  }
+  // Send the input to the conversation service
+  conversation.message( payload, function(err, data) {
+    if ( err ) {
+      return res.status( err.code || 500 ).json( err );
+    }
+    return res.json( updateMessage( payload, data ) );
+  } );
+} );
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  var id = null;
+  if ( !response.output ) {
+    response.output = {};
+  } else {
+    if ( logs ) {
+      // If the logs db is set, then we want to record all input and responses
+      id = uuid.v4();
+      logs.insert( {'_id': id, 'request': input, 'response': response, 'time': new Date()});
+    }
+    return response;
+  }
+  if ( response.intents && response.intents[0] ) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if ( intent.confidence >= 0.75 ) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if ( intent.confidence >= 0.5 ) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  if ( logs ) {
+    // If the logs db is set, then we want to record all input and responses
+    id = uuid.v4();
+    logs.insert( {'_id': id, 'request': input, 'response': response, 'time': new Date()});
+  }
+  return response;
+}
